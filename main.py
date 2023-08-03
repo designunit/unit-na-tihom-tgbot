718cb9eb27fe9fff8ab11a23667e712309c1ce9b
modernpacifist <modernpacifist@gmail.com>
2023-08-03 18:20:30 +0300

    init

---
+++ main.py
@@ -0,0 +1,14 @@
+#!/bin/env python3.10
+from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove
+from telegram.ext import (
+    ApplicationBuilder,
+    CommandHandler,
+    MessageHandler,
+    ConversationHandler,
+    filters,
+)
+
+
+if __name__ == '__main__':
+    print("hello world")
+    exit(0)
