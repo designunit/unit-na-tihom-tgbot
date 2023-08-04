73e7cd4697b9d9ea3f4acf3a48edc939eea59311
modernpacifist <modernpacifist@gmail.com>
2023-08-04 05:55:01 +0300

    proelium-exercitum:04/08/2023-05:55:01 automated backup commit

--- main.py
+++ main.py
@@ -1,14 +1,54 @@
 #!/bin/env python3.10
-from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove
+import telegram
+
 from telegram.ext import (
-    ApplicationBuilder,
     CommandHandler,
-    MessageHandler,
+    ApplicationBuilder,
     ConversationHandler,
-    filters,
 )
 
 
+async def map_start_conversation(context, update):
+    return None
+
+
+async def map_stop_conversation(context, update):
+    return None
+
+
+def main():
+    try:
+        app = ApplicationBuilder().token(config.BOT_TOKEN).build()
+    except Exception as e:
+        LOGGER.critical(f"{e}")
+        exit(1)
+
+    app.add_handler(CommandHandler("start", help))
+
+    revert_hander = ConversationHandler(
+        entry_points=[CommandHandler("revert", revert)],
+        states={
+            USER_KEYBOARD_INPUT: [
+                MessageHandler(
+                    filters.Regex("^(Yes|No)$"),
+                    confirm_revert
+                )
+            ],
+        },
+        fallbacks=[CommandHandler("cancel", cancel)],
+    )
+
+    app.add_handler(insert_record_handler)
+    app.add_handler(revert_hander)
+
+    app.add_handler(MessageHandler(filters.TEXT, help))
+
+    app.add_error_handler(error)
+
+    # start
+    app.run_polling()
+
+
 if __name__ == '__main__':
-    print("hello world")
-    exit(0)
+    main()
+
