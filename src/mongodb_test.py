95d7e397d2497d3b2b2e43379a9ea3fa13648f99
DanilaLabydin <labydin2605@mail.ru>
2023-08-06 12:28:37 +0300

    test storing files in mongo

--- src/mongodb_test.py
+++ src/mongodb_test.py
@@ -1,6 +1,8 @@
 import datetime
+import gridfs
 
-from mongo_ops import insert_event, get_events_by_user_time
+
+from mongo_ops import insert_event, get_events_by_user_time, connect
 
 
 event_sample1 = {
@@ -22,10 +24,32 @@ event_sample2 = {
 }
 
 
-insert_event(event_sample1)
-insert_event(event_sample2)
-test_time = datetime.datetime(2023, 8, 11, 15, 00)
-test = get_events_by_user_time(test_time)
+# insert_event(event_sample1)
+# insert_event(event_sample2)
+# test_time = datetime.datetime(2023, 8, 11, 15, 00)
+# test = get_events_by_user_time(test_time)
 
-for i in test:
-    print(i)
+# for i in test:
+#     print(i)
+
+conn = connect()
+db = conn['tixiy_bot_db']
+fs = gridfs.GridFS(db)
+
+name = 'unit на тихом 2023.pdf'
+file_location = '/home/danila/Documents/' + name
+
+# with open(file_location, 'rb') as file:
+#     data = file.read()
+
+# fs.put(data, filename=name)
+# print('upload completed')
+
+name2 = 'test123.pdf'
+new_data = db.fs.files.find_one({"filename": name})
+my_id = new_data['_id']
+output_data = fs.get(my_id).read()
+download_location = '/home/danila/Downloads/' + name2
+with open(download_location, 'wb') as file:
+    file.write(output_data) 
+print("download completed")
