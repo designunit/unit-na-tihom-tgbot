434514d092c31fdf26a0a3ab40700cfc88edd30c
DanilaLabydin <labydin2605@mail.ru>
2023-08-07 11:30:07 +0300

    add basic functions and buttons (without logic yet)

--- src/main.py
+++ src/main.py
@@ -6,7 +6,12 @@ from telegram.ext import (
     CommandHandler,
     ApplicationBuilder,
     ConversationHandler,
+    MessageHandler,
+    filters,
+    ContextTypes
 )
+
+from telegram import ForceReply, ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardButton
 
 # local imports
 import config
@@ -15,12 +20,56 @@ import config
 LOGGER = logging.getLogger(__name__)
 
 
-async def map_start_conversation(context, update):
-    return None
+COMMAND_KEYBOARD = ReplyKeyboardMarkup(
+    keyboard=[
+         [KeyboardButton(text='карта'),KeyboardButton(text='что происходит')],
+         [KeyboardButton(text='программа'), KeyboardButton(text='музыка')],
+         [KeyboardButton(text='правила лагеря'),KeyboardButton(text='ландшафтные объекты')],
+         [KeyboardButton(text='важное'), KeyboardButton(text='трансфер')]
+         ],
+    resize_keyboard=True,
+    is_persistent=True,
+)
 
 
-async def map_stop_conversation(context, update):
-    return None
+async def start(update, context):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="choose an option", reply_markup=COMMAND_KEYBOARD)
+
+
+async def get_map(update, context):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="sent you your map!")
+
+
+async def get_current_events(update, context):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="sent you current events")
+
+
+async def get_programm(update, context):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="sent you your programm")
+
+
+async def get_music(update, context):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="sent you your music")
+
+
+async def get_camp_rules(update, context):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="sent you the camp rules")
+
+
+async def get_landscape_objects(update, context):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="sent you thr")
+
+
+async def get_important_info(update, context):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="important info!")
+
+
+async def get_transer_info(update, context):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="transer!")
+
+
+async def help_command(update, context):
+    await update.message.reply_text("Help!")
 
 
 def main():
@@ -30,27 +79,16 @@ def main():
         LOGGER.critical(f"{e}")
         exit(1)
 
-    app.add_handler(CommandHandler("start", help))
+    app.add_handler(CommandHandler("start", start))
 
-    revert_hander = ConversationHandler(
-        entry_points=[CommandHandler("revert", revert)],
-        states={
-            USER_KEYBOARD_INPUT: [
-                MessageHandler(
-                    filters.Regex("^(Yes|No)$"),
-                    confirm_revert
-                )
-            ],
-        },
-        fallbacks=[CommandHandler("cancel", cancel)],
-    )
-
-    app.add_handler(insert_record_handler)
-    app.add_handler(revert_hander)
-
-    app.add_handler(MessageHandler(filters.TEXT, help))
-
-    app.add_error_handler(error)
+    app.add_handler(MessageHandler(filters.TEXT & filters.Regex("^карта$"), get_map))
+    app.add_handler(MessageHandler(filters.TEXT & filters.Regex("^что происходит$"), get_current_events))
+    app.add_handler(MessageHandler(filters.TEXT & filters.Regex("^программа$"), get_programm))
+    app.add_handler(MessageHandler(filters.TEXT & filters.Regex("^музыка$"), get_music))
+    app.add_handler(MessageHandler(filters.TEXT & filters.Regex("^правила лагеря$"), get_camp_rules))
+    app.add_handler(MessageHandler(filters.TEXT & filters.Regex("^ландшафтные объекты$"), get_landscape_objects))
+    app.add_handler(MessageHandler(filters.TEXT & filters.Regex("^важное$"), get_important_info))
+    app.add_handler(MessageHandler(filters.TEXT & filters.Regex("^трансфер"), get_transer_info))
 
     # start
     app.run_polling()
