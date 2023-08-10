81fc9411dff58c6db790bbe18b76ef225e1562ae
modernpacifist <modernpacifist@gmail.com>
2023-08-10 17:23:16 +0300

    updated .gitignore/updated transfer function/updated file upload script for mongodb

--- src/main.py
+++ src/main.py
@@ -223,10 +223,19 @@ async def get_important_info(update, context):
 
 
 async def get_transer_info(update, context):
+    file_data = mongo_ops.get_file_by_name("transfer_jpg")
+    if file_data is None:
+        return
+
     await context.bot.send_message(
-        chat_id=update.effective_chat.id, 
-        text="трансфер:",
-        reply_markup=TRANSFER_KEYBOARD
+        chat_id=update.effective_chat.id,
+        text="Трансфер:",
+    )
+    file_name, presentation = file_data
+    await context.bot.send_document(
+        chat_id=update.effective_chat.id,
+        document=presentation,
+        filename=file_name,
     )
 
 
--- src/mongodb_fileupload.py
+++ src/mongodb_fileupload.py
@@ -1,61 +1,36 @@
-import datetime
+import sys
 import gridfs
+import argparse
 
 
-from mongo_ops import insert_event, get_events_by_user_time, connect
+from mongo_ops import connect
 
 
-event_sample1 = {
-    "time_start": datetime.datetime(2023, 8, 8, 13, 00),
-    "time_end": datetime.datetime(2023, 8, 8, 15, 00),
-    "name": "музыка",
-    "mark": "Брифинг",
-    "speaker": [
-        {"name": "Лиза Владимировна", "status": "Куратор резиденции"},
-        {"name": "Владимир Петросян", "status": "Куратор резиденции"},
-    ],
-}
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
 
+    parser.add_argument('-f', '--file', required=True, help='file location')
+    parser.add_argument('-l', '--label', required=True, help='file label')
 
-# event_sample2 = {
-#     "time_start": datetime.datetime(2023, 8, 11, 14, 00),
-#     "time_end": datetime.datetime(2023, 8, 11, 16, 00),
-#     "Name": "LE BEAU SERGE",
-# }
+    args = parser.parse_args()
 
+    conn = connect()
 
-# insert_event(event_sample1)
-# insert_event(event_sample2)
-# test_time = datetime.datetime(2023, 8, 11, 15, 00)
-# test = get_events_by_user_time(test_time)
+    db = conn["tixiy_bot_db"]
+    fs = gridfs.GridFS(db)
 
-# for i in test:
-#     print(i)
+    name = args.file
+    name_label = args.label
 
+    file_location = f"/mnt/c/Users/modernpacifist/Documents/github-repositories/unit-na-tihom-tgbot/separate_events_jpg/{name}"
 
-## ADD FILE
+    try:
+        with open(file_location, "rb") as file:
+            data = file.read()
 
-conn = connect()
-db = conn["tixiy_bot_db"]
-fs = gridfs.GridFS(db)
+        fs.put(data, label=name_label, filename=file.name)
+        print("upload completed")
 
-name = "music.pdf"
-name_label = "music_pdf"
-
-file_location = "/mnt/c/Users/modernpacifist/Documents/github-repositories/unit-na-tihom-tgbot/" + name
-
-with open(file_location, "rb") as file:
-    data = file.read()
-
-
-fs.put(data, label=name_label, filename=name)
-print("upload completed")
-
-# name2 = 'test123.jpj'
-# new_data = db.fs.files.find_one({"filename": name})
-# my_id = new_data['_id']
-# output_data = fs.get(my_id).read()
-# download_location = '/home/danila/Downloads/' + name2
-# with open(download_location, 'wb') as file:
-#     file.write(output_data)
-# print("download completed")
+    except Exception as e:
+        print(e)
+        exit(1)
